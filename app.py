6ad9aa74098fb373a005a3ee4d16437f84bd0be6
Mike Resoli <mike.resoli@gmail.com>
2017-03-18 15:37:28 +0000

    set up project query

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route("/project/<int:projectid>")
 def project(projectid):
-	database.projects.query.filter_by(id=project).first()
+	project = database.projects.query.filter_by(id=project).first()
 	return "project"
 
 @app.route("/project/<int:projectid>/<int:eventid>")
