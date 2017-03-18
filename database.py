3828bd40c126cd4333e575c78c1335a761806e93
Mike Resoli <mike.resoli@gmail.com>
2017-03-18 23:25:20 +0000

    remove dead packages

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, fields
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
