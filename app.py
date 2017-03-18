f8b27995c1c4e04b6c18021cbc7c9e8da907180b
Mike Resoli <mike.resoli@gmail.com>
2017-03-18 13:33:55 +0000

    add routes

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
 	name = "hack24";
 	return render_template('index.html', name=name)
+
+@app.route("/project/<int:projectid>")
+def project():
+	return "project"
+
+@app.route("/project/<int:projectid>/<int:eventid>")
+def project():
+	return "project -> event"
 
 if __name__ == "__main__":
 	app.run()
