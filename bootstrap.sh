fc33645b31825d35a7e98ef2726ce1bddda5154c
Alistair Shaw <alistairshaw@gmail.com>
2017-03-18 10:44:45 +0000

    Fix for Python3

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,23 +4,22 @@ apt-get update
 
 apt-get install build-essential libssl-dev libffi-dev python3-dev
 
-pip install --upgrade pip3
-pip install Flask
+pip3 install --upgrade pip3
+pip3 install Flask
 
 cd /vagrant
-export FLASK_DEBUG=1
-export FLASK_APP=app.py
 
 echo "
 ****************************
 ALL DONE
 ****************************
 
-Umm yeah it should be running now.
+Umm yeah it should be up now.
 
+vagrant ssh
+cd /vagrant
+export FLASK_DEBUG=1; export FLASK_APP=app.py
 You can access the Hack24 app on http://192.168.10.200:5000/
 
 ****************************
 "
-
-flask run --host=0.0.0.0
